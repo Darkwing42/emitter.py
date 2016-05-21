44cfd910b91d22a492f66c5476f78d8315f3f206
Titouan de Kerautem <tdekerautem@genymobile.com>
2016-05-21 22:32:42 +0200

    add setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'emitter',
+  packages = ['emitter'],
+  version = '1.0',
+  description = 'A minimalist event emitter for Python 3.',
+  author = 'titouandk',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/titouandk/emitter.py',
+  download_url = 'https://github.com/titouandk/emitter.py/tarball/v1.0',
+  keywords = ['event emitter', 'event', 'emitter', 'python 3', 'python3'],
+  classifiers = [],
+)

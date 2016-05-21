c110bdb28dcacd3ebbf4c7866fa591e87d238cee
Titouan de Kerautem <tdekerautem@genymobile.com>
2016-05-21 22:34:06 +0200

    update email setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   version = '1.0',
   description = 'A minimalist event emitter for Python 3.',
   author = 'titouandk',
-  author_email = 'peterldowns@gmail.com',
+  author_email = 'dk.titouan@gmail.com',
   url = 'https://github.com/titouandk/emitter.py',
   download_url = 'https://github.com/titouandk/emitter.py/tarball/v1.0',
   keywords = ['event emitter', 'event', 'emitter', 'python 3', 'python3'],

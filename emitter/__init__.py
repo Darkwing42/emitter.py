008c51dc90bb0b3c9516998be70c2360719bf6b8
Titouan de Kerautem <tdekerautem@genymobile.com>
2016-08-22 22:47:27 +0200

    api template - methods signatures

--- emitter/__init__.py
+++ emitter/__init__.py
@@ -4,29 +4,27 @@ __version__ = "6.0.0"
 
 
 class Emitter:
+    ERROR = object()
+    ATTACH = object()
+    DETACH = object()
+
     def __init__(self):
         pass
 
-    def on(self, event, listener, error_handler=None):
+    def on(self, event, listener):
         pass
 
-    def once(self, event, listener, error_handler=None):
+    def once(self, event, listener):
         pass
 
     def emit(self, event, *args, **kwargs):
         pass
 
-    def clear(self, event=None, listener=None, error_handler=None):
-        pass
-
-    def get(self, event=None, listener=None):
+    def off(self, event=None, listener=None):
         pass
 
     def events(self):
         pass
 
     def listeners(self, event):
-        pass
-
-    def on_error(self, error_handler):
         pass

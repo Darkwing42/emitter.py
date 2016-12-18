e05dd94222ea32c2675b99201e2306af6a7ab1e0
Titouan de Kerautem <tdekerautem@genymobile.com>
2016-12-18 20:34:59 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
   author_email = 'dk.titouan@gmail.com',
   url = 'https://github.com/titouandk/emitter.py',
   download_url = 'https://github.com/titouandk/emitter.py/tarball/7.0.0',
-  keywords = ['event emitter', 'event', 'emitter', 'python 3', 'python3'],
+  keywords = ['emitter.py', 'event emitter', 'event', 'emitter', 'python 3', 'python3'],
   classifiers = ['Programming Language :: Python :: 3'],
 )
